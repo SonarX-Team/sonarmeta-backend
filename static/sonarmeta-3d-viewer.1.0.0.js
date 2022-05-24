44480be3a10a637c74b56981f565b8addc766840
Archimesons <7592301+archimesons@user.noreply.gitee.com>
2022-05-24 15:21:05 +0800

    cancel stater

--- static/sonarmeta-3d-viewer.1.0.0.js
+++
@@ -1,6 +0,0 @@
-// © 2022 SONARMETA 声呐元
-// SonarX声呐探索, SonarX .Inc
-const sonarmeta = (a, b, c) => {
-  c.src = "https://www.sonarmeta.com" + "/3d-viewer/resource" + `/${b}` + `?secretKey=${a}`;
-};
-export default sonarmeta;
